be0e0f867c3583ad5311881103a9cd91c28f4bf8
Ramon Adan <dann.tesla@gmail.com>
2024-10-27 18:36:55 -0600

    exponiendo rutas, uso de rutas absolutas y relativas

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -12,4 +12,17 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    // Ruta completa que comienza desde la raiz de un crate; para el codigo
+    // de un crate externo, la ruta absoluta comienza con el nombre del crate,
+    // y para el codigo de un crate actual, comienza con el crate literal.
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    // Comienza desde le modulo actual y utiliza self, super, o un identificador
+    // del modulo actual.
+    front_of_house::hosting::add_to_waitlist();
 }
