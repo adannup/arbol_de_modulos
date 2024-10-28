b009e74ca92271c210e6085e141f975f38fc34c2
Ramon Adan <dann.tesla@gmail.com>
2024-10-27 22:46:17 -0600

    uso de super

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,20 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    fn fix_incorrect_order() {
+        // Usar super nos permite hacer referencia a un item que sabemos que esta en el
+        // modulo padre, lo que puede facilitar la reorganizacion del arbol de modulos
+        // cuando el modulo esta estrechamente relacionado con el padre.
+        super::deliver_order();
+        cook_order();
+    }
+
+    fn cook_order() {}
+}
+
+fn deliver_order() {}
+
 pub fn eat_at_restaurant() {
     // absolute path
     // Ruta completa que comienza desde la raiz de un crate; para el codigo
